ea932c137d7964f1e1e4b92c7deb6db570c385d2
Marcel <nouletamemarcel@gmail.com>
2022-11-07 16:01:59 +0000

    notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/services.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 
 import 'presentations/screens/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await FirebaseMessaging.instance.getInitialMessage();
+  /*  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+ */
   final storage = await HydratedStorage.build(
       storageDirectory: await getApplicationDocumentsDirectory());
 
--- lib/presentations/screens/login_screen.dart
+++ lib/presentations/screens/login_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:validators/validators.dart';
-
+import 'package:firebase_messaging/firebase_messaging.dart';
 import '../widgets/button.dart';
 import '../widgets/password_input_field.dart';
 import '../widgets/text_input_filed.dart';
@@ -21,8 +24,11 @@ class _LoginScreenState extends State<LoginScreen> {
     'password': '',
   };
 
+  String? mtoken = '';
+  late FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
   void _submit() {
-    setState(() {
+    /* setState(() {
       _autovalidateMode = AutovalidateMode.always;
     });
 
@@ -30,7 +36,107 @@ class _LoginScreenState extends State<LoginScreen> {
 
     if (form == null || !form.validate()) return;
 
-    form.save();
+    form.save(); */
+  }
+
+  requestPermission() async {
+    FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+    NotificationSettings settings = await messaging.requestPermission(
+      alert: true,
+      announcement: false,
+      badge: true,
+      carPlay: false,
+      criticalAlert: false,
+      provisional: false,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('user granted permission');
+    } else if (settings.authorizationStatus ==
+        AuthorizationStatus.provisional) {
+      print('user granted provisional permission');
+    } else {
+      print('user declined or has not accepted permission');
+    }
+  }
+
+  getToken() async {
+    await FirebaseMessaging.instance.getToken().then((token) {
+      setState(() {
+        mtoken = token;
+        print('My token is $mtoken');
+      });
+      saveToken(mtoken!);
+    });
+  }
+
+  void saveToken(String token) async {
+    await FirebaseFirestore.instance
+        .collection("UserToken")
+        .doc("User1")
+        .set({'token': token});
+  }
+
+  initInfo() {
+    var androidInitilize =
+        const AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettings =
+        InitializationSettings(android: androidInitilize);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onDidReceiveNotificationResponse:
+            (NotificationResponse notificationResponse) {
+      try {
+        if (notificationResponse.notificationResponseType ==
+            NotificationResponseType.selectedNotification) {}
+      } catch (e) {}
+      /* switch (notificationResponse.notificationResponseType) {
+        case NotificationResponseType.selectedNotification:
+          //selectNotificationStream.add(notificationResponse.payload);
+          break;
+        case NotificationResponseType.selectedNotificationAction:
+          if (notificationResponse.actionId == navigationActionId) {
+            //selectNotificationStream.add(notificationResponse.payload);
+          }
+          break;
+      } */
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      print('***********************************************');
+      print('Message data: ${message.data}');
+
+      BigTextStyleInformation bigTextStyleInformation = BigTextStyleInformation(
+        message.notification!.body.toString(),
+        htmlFormatBigText: true,
+        contentTitle: message.notification!.title.toString(),
+        htmlFormatContent: true,
+      );
+      AndroidNotificationDetails androidPlatformSpecifics =
+          AndroidNotificationDetails(
+        'dbfood',
+        'dbfood',
+        importance: Importance.high,
+        styleInformation: bigTextStyleInformation,
+        priority: Priority.high,
+        playSound: false,
+      );
+
+      NotificationDetails platformChannelSpecifics =
+          NotificationDetails(android: androidPlatformSpecifics);
+      await flutterLocalNotificationsPlugin.show(0, message.notification?.title,
+          message.notification?.body, platformChannelSpecifics,
+          payload: message.data['body']);
+    });
+  }
+
+  @override
+  void initState() {
+    requestPermission();
+    getToken();
+    initInfo();
+    super.initState();
   }
 
   @override
